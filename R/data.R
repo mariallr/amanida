f724cc5aac521c1e1527302013f9edc237115e03
eudald <eudald@correig.net>
2021-02-15 16:06:45 +0100

    better csv treatment

--- R/data.R
+++ R/data.R
@@ -11,13 +11,22 @@ data.read <- function(file, coln, separator=NULL) {
   
   library(dplyr)
   
+  ext <- tools::file_ext(file)
+  
   # Read file using appropiate extension
-  if (length(grep("\\.csv|\\.txt", file)) > 0) {
-    datafile <- readr::read_delim(file, sep = separator)
-  } else if (length(grep("\\.xlsx|\\.xls", file)) > 0) {
+  if (ext %in% c("csv", "tsv", "txt")) {
+    stopifnot("Please, specify a separator."=!is.null(separator))
+    
+    datafile <- readr::read_delim(file, delim = separator) %>%
+      # In some (specially spanish) locales, when the delim is ",", the decimal
+      # point is ","; let's make sure here this is correct
+      mutate(
+        across(c("P-value", "Fold-change"), function(x) sub(",", ".", x, fixed = TRUE))
+      )
+  } else if (ext %in% c("xlsx", "xls")) {
     datafile <- readxl::read_excel(file)
   } else {
-    stop("Format not compatible; try csv, excel or txt. Aborting.")
+    stop("Format not compatible; try csv, tsv, excel or txt. Aborting.")
   }
   
   datafile %>%
@@ -25,13 +34,21 @@ data.read <- function(file, coln, separator=NULL) {
     select(all_of(coln)) %>%
     rename_with(.cols = everything(), .fn = ~ VAR_NAMES) %>%
     mutate(
+      # Make sure numeric things are numeric
+      foldchange = as.numeric(foldchange),
+      pvalue = as.numeric(pvalue),
+      N = as.integer(N),
       # Inverted fold-change for negative values
-      foldchange = case_when(foldchange < 0 ~ 1 / abs(foldchange),
-                             T ~ foldchange),
+      foldchange = case_when(
+        foldchange < 0 ~ 1 / abs(foldchange),
+        T ~ foldchange
+        ),
       # Add trend column
-      trend = case_when(foldchange < 1 ~ -1,
-                        foldchange == 1 ~ 0,
-                        T ~ 1)
+      trend = case_when(
+        foldchange < 1 ~ -1,
+        foldchange == 1 ~ 0,
+        T ~ 1
+        )
     )
   
 }
