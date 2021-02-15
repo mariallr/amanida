b8ebfbeb2433c47656cfb30a1f6a21616bd96b2f
eudald <eudald@correig.net>
2021-02-15 13:16:49 +0100

    convert to dplyr

--- R/data.R
+++ R/data.R
@@ -1,58 +1,37 @@
 ## Load data
 
+
+VAR_NAMES <- c("id", "pvalue", "foldchange", "N", "ref")
+
+
 data.read <- function(file, separator, coln) {
-  
   # Function: read data files
   # Arguments: filename, separator symbol, columns to import
   # Return: tibble table with data imported
   
   library(dplyr)
   
-  # Search file extension
-  
-  csv <- grep("\\.csv", file)
-  excel <- grep("\\.xlsx|\\.xls", file)
-  txt <- grep("\\.txt", file)
-  
   # Read file using appropiate extension
-  if (length(csv) == 1) {
-    datafile <- read.csv(file, sep = separator)
-  } else if (length(excel) == 1) {
+  if (length(grep("\\.csv|\\.txt", file)) > 0) {
+    datafile <- readr::read_delim(file, sep = separator)
+  } else if (length(grep("\\.xlsx|\\.xls", file)) > 0) {
     datafile <- readxl::read_excel(file)
-  } else if (length(txt) == 1) {
-    datafile <- read.table(file, sep = separator)
   } else {
-    print("Not compatible format, try csv, excel or txt")
+    stop("Format not compatible; try csv, excel or txt. Aborting.")
   }
   
-  # Convert data imported as tibble
-  datafile <- dplyr::as_tibble(datafile)
-  
-  # Select columns with data needed
-  datafile <- datafile %>% select(all_of(coln)) 
-  
-  # Rename columns
-  colnames(datafile) <- c("id", "pvalue", "foldchange", "N", "ref")
-  
-  # Add trend column
-  if (length(datafile$foldchange[datafile$foldchange < 0]) > 0) { 
-    
-    # Inverted fold-change for negative values 
-    datafile$foldchange[datafile$foldchange < 0] <- 
-      1 / abs(datafile$foldchange[datafile$foldchange < 0] )
-    
-    # Negative values
-    datafile[datafile$foldchange < 1, "trend"] <- -1 
-    
-    # Positive values
-    datafile[datafile$foldchange > 1, "trend"] <- 1 
-    
-    # No behaviour change  
-    datafile[datafile$foldchange == 1, "trend"] <- 0
-    
-  }
-  
-  
-  return(datafile)
+  datafile %>%
+    # Select columns with data needed and rename
+    select(all_of(coln)) %>%
+    rename_with(.cols = everything(), .fn = ~ VAR_NAMES) %>%
+    mutate(
+      # Inverted fold-change for negative values
+      foldchange = case_when(foldchange < 0 ~ 1 / abs(foldchange),
+                             T ~ foldchange),
+      # Add trend column
+      trend = case_when(foldchange < 1 ~ -1,
+                        foldchange == 1 ~ 0,
+                        T ~ 1)
+    )
   
 }
