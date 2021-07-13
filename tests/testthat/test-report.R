3a5ed2d4c92e8ba58869bd8f0a21a96ba8adb854
Maria Llambrich <maria.llambrich@urv.cat>
2021-07-13 11:14:13 +0200

    corrected test

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -2,7 +2,7 @@ context("Report qualitative meta-analysis")
 
 test_that("Report function", {
   local_edition(3)
-  coln = c("Compound Name", "Behaviour", "References")
+  column_id = c("Compound Name", "P-value", "Fold-change", "N total", "References")
   input_file <- system.file("extdata", "dataset2.csv", package = "amanida")
   
   expect_snapshot(amanida_report(input_file, separator = ";", column_id, analysis_type = "quan", pvalue_cutoff = 0.05, fc_cutoff = 4, votecount_lim = 2))
