63f56366e096729e5f5f4702b7db199804a49faf
Maria Llambrich <maria.llambrich@urv.cat>
2021-07-19 17:35:55 +0200

    test for new report

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -1,6 +1,6 @@
-context("Report qualitative meta-analysis")
+context("Report meta-analysis")
 
-test_that("Report function", {
+test_that("Report function quan", {
   local_edition(3)
   column_id = c("Compound Name", "P-value", "Fold-change", "N total", "References")
   input_file <- system.file("extdata", "dataset2.csv", package = "amanida")
@@ -8,6 +8,12 @@ test_that("Report function", {
   expect_snapshot(amanida_report(input_file, separator = ";", column_id, analysis_type = "quan", pvalue_cutoff = 0.05, fc_cutoff = 4, votecount_lim = 2))
 })
 
-
+test_that("Report function qual", {
+  local_edition(3)
+  column_id = c("Compound Name", "Behaviour", "References")
+  input_file <- system.file("extdata", "dataset2.csv", package = "amanida")
+  
+  expect_snapshot(amanida_report(input_file, separator = ";", column_id, analysis_type = "qual", votecount_lim = 2))
+})
 
 
