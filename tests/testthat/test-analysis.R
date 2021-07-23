bc2f2f6e3cc2e1fe91fe87fa37a793d2519b3b97
Maria Llambrich <maria.llambrich@urv.cat>
2021-07-23 13:18:51 +0200

    new method for fisher

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -8,7 +8,7 @@ test_that("Quantitative analysis of data", {
   
   expect_equal(result@stat$fc[8], 0.6389)
   expect_equal(result@stat$N_total[6], 342)
-  expect_equal(result@stat$pval[2], 0.1784337)
+  expect_equal(result@stat$pval[2], 0.0189)
   
   expect_equal(result@vote$votes[100], -1)
   expect_equal(result@vote$articles[48], 1)
