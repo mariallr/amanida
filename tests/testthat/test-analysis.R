ab24e5477a6d783035037d1428efa7082a0d300d
mariall <mllambrichr@gmail.com>
2024-01-24 16:03:29 +0100

    updated tests

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -5,20 +5,20 @@ test_that("Quantitative analysis of data", {
  
   data("sample_data")
   
-  result <- compute_amanida(sample_data)
+  result <- compute_amanida(sample_data, comp.inf = F)
   
-  expect_equal(result@stat %>% filter(id == "2-Ethyl-5-methylfuran") %>% 
-                 pull(fc), 0.6389, tolerance = 0.01)
-  expect_equal(result@stat %>% filter(id == "Fumaric acid") %>% 
-                 pull(N_total), 204)
-  expect_equal(result@stat %>% filter(id == "Phenylacetic acid") %>% 
-                 pull(pval), 4.636927e-05)
+  expect_equal(result@stat %>% dplyr::filter(id == "2-Ethyl-5-methylfuran") %>% 
+                 dplyr::pull(fc), 0.6389, tolerance = 0.01)
+  expect_equal(result@stat %>% dplyr::filter(id == "Fumaric acid") %>% 
+                 dplyr::pull(N_total), 204)
+  expect_equal(result@stat %>% dplyr::filter(id == "Phenylacetic acid") %>% 
+                 dplyr::pull(pval), 4.636927e-05)
   
-  expect_equal(result@vote %>% filter(id == "3-Phosphoglycerate") %>% 
-                 pull(votes), -1)
-  expect_equal(result@vote %>% filter(id == "L-Tryptophan") %>% 
-                 pull(articles), 3)
-  expect_equal(result@vote %>% filter(id == "Tiglic aldehyde") %>% 
-                 pull(vote_counting), -1)
+  expect_equal(result@vote %>% dplyr::filter(id == "3-Phosphoglycerate") %>% 
+                 dplyr::pull(votes), -1)
+  expect_equal(result@vote %>% dplyr::filter(id == "L-Tryptophan") %>% 
+                 dplyr::pull(articles), 3)
+  expect_equal(result@vote %>% dplyr::filter(id == "Tiglic aldehyde") %>% 
+                 dplyr::pull(vote_counting), -1)
 })
 
