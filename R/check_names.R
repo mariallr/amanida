f247db421659bb4f9c53aea8c25c4efd041db38e
mariall <mllambrichr@gmail.com>
2024-01-25 16:04:02 +0100

    description

--- R/check_names.R
+++ R/check_names.R
@@ -8,8 +8,8 @@
 #'
 #' Formats compatible are \code{amanida_read} output
 #' 
-#' @param datafile data imported using \code{amanida_read} function
-#' @return tibble table with data imported
+#' @param data data imported using \code{amanida_read} function
+#' @return tibble table with data imported with PubChem ID retrieved
 #' 
 #' @import dplyr
 #' @import webchem
@@ -24,29 +24,29 @@
 #' }
 #' 
 #' @export
-#' 
-check_names <- function(datafile){
+
+check_names <- function(data) {
   id_mod = NULL;
   
-  datafile <- datafile |> mutate(cid = NA, id_mod = NA)
+  data <- data |> mutate(cid = NA, id_mod = NA)
   
-  for(i in 1:length(datafile$id)){
-    a <- get_cid(datafile$id[i], 
+  for(i in 1:length(data$id)){
+    a <- get_cid(data$id[i], 
                  from = "name",
                  domain = c("compound", "substance", "assay"), 
                  match = "first")
     if(is.na(a$cid)){
-      a <- get_cid(datafile$id[i], 
+      a <- get_cid(data$id[i], 
                    from = "inchikey",
                    domain = c("compound", "substance", "assay"),
                    match = "first")
       if(is.na(a$cid)){
-        a <- get_cid(datafile$id[i], 
+        a <- get_cid(data$id[i], 
                      from = "inchi",
                      domain = c("compound", "substance", "assay"), 
                      match = "first")
         if(is.na(a$cid)){
-          a <- get_cid(datafile$id[i], 
+          a <- get_cid(data$id[i], 
                        from = "smiles",
                        domain = c("compound", "substance", "assay"), 
                        match = "first")
@@ -54,13 +54,15 @@ check_names <- function(datafile){
       } 
     } 
     
-    datafile$cid[i] <- a$cid
+    data$cid[i] <- a$cid
     
-    datafile$id_mod[i] <- pc_synonyms(a$cid, 
-                                      from = "cid", 
-                                      match = "first")
+    data$id_mod[i] <- pc_synonyms(a$cid, 
+                                  from = "cid", 
+                                  match = "first")
   }
   
-  datafile <- datafile |> 
+  data <- data |> 
     mutate("id_mod" = ifelse(is.na(id_mod), id, id_mod))
+  
+  return(data)
 }
